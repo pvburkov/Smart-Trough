af66cb5a987c844469c65187f3673e54e71f2494
Павел Бурков < >
2017-05-11 01:06:56 +0400

    Telebot v2.0: reactions to most commands added

--- photobot.py
+++ photobot.py
@@ -1,5 +1,8 @@
-import telebot
 import os
+import os.path
+import random
+import telebot
+import time
 import unittest
 
 class PhotoBot():
@@ -21,17 +24,61 @@ class PhotoBot():
         """
         метод класса PhotoBot, который загружает фотографию в чат-бот Telegram
         """
-        tb = telebot.TeleBot(self.token)
+        bot = telebot.TeleBot(self.token)
         
-        @tb.message_handler(content_types=["text"])
-        def photo_for_message(message):
+        @bot.message_handler(commands = ['start'])
+        def handle_start(message):
+            bot.send_message(message.chat.id, 'Hello! Funny birds are greeting you here!')
+
+        @bot.message_handler(commands = ['new'])
+        def handle_new(message):
+            new_file_time = 0
             for file in os.listdir(self.photo_dir):
-                if file.split('.')[1] == 'jpg':
-                    photo = open(self.photo_dir + '\\' + file, 'rb')
-                    tb.send_photo(message.chat.id, photo)
+                file_path = self.photo_dir + '\\' + file
+                if file.split('.')[1] == 'jpg' and os.path.getmtime(file_path) > new_file_time:
+                    new_file = file_path
+                    new_file_time = os.path.getmtime(file_path)
+            photo = open(new_file, 'rb')
+            bot.send_photo(message.chat.id, photo)
+            photo.close()
+
+        @bot.message_handler(commands = ['old'])
+        def handle_old(message):
+            old_file_time = 20000000000
+            for file in os.listdir(self.photo_dir):
+                file_path = self.photo_dir + '\\' + file
+                if file.split('.')[1] == 'jpg' and os.path.getmtime(file_path) < old_file_time:
+                    old_file = file_path
+                    old_file_time = os.path.getmtime(file_path)
+            photo = open(old_file, 'rb')
+            bot.send_photo(message.chat.id, photo)
+            photo.close()
+
+        @bot.message_handler(commands = ['random'])
+        def handle_random(message):
+            while True:
+                rand_file = random.choice(os.listdir(self.photo_dir))
+                if rand_file.split('.')[1] == 'jpg':
+                    break
+            photo = open(self.photo_dir + '\\' + rand_file, 'rb')
+            bot.send_photo(message.chat.id, photo)
+            photo.close()
+
+        @bot.message_handler(commands = ['hour'])
+        def handle_hour(message):
+            timestamp = time.time()
+            for file in os.listdir(self.photo_dir):
+                file_path = self.photo_dir + '\\' + file
+                if file.split('.')[1] == 'jpg' and timestamp - os.path.getmtime(file_path) < 3600:
+                    photo = open(file_path, 'rb')
+                    bot.send_photo(message.chat.id, photo)
                     photo.close()
+
+        @bot.message_handler(content_types=["text"])
+        def handle_message(message):
+            bot.send_message(message.chat.id, 'Tweet! Tweet-tweet! Tweet!')
         
-        tb.polling(none_stop=True)
+        bot.polling(none_stop=True)
 
 class Test_PhotoBot(unittest.TestCase):
     """
